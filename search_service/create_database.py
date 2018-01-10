101320c6954650190305c1f406fc7e8320cc6c42
feger <marc.feger@hhu.de>
2018-01-10 13:42:10 +0100

    Change debug string in create_database

--- search_service/create_database.py
+++ search_service/create_database.py
@@ -14,13 +14,13 @@ def seed_database():
     :return:
     """
 
-    logging.debug(":: Test connection to elastic search is active")
+    logging.debug("Test connection to elastic search is active")
     es = create_connection()
     logging.debug("Connection is established: {0}".format(es.ping()))
 
-    logging.debug(":: Fill elastic_search database")
+    logging.debug("Fill elastic_search database")
     init_database(es)
-    logging.debug("Connection is established: {0}".format(es.ping()))
+    logging.debug("Connection is established after seeding: {0}".format(es.ping()))
     content = es.get(index=INDEX_NAME, doc_type=DOC_TYPE, id=0)
     logging.debug("Content is set: {0}".format(content is not None))
 
