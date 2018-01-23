501bd85fd4d07e28eaae7c27af00342996f44972
feger <marc.feger@hhu.de>
2018-01-23 13:21:00 +0100

    Fix tests

--- search_service/tests/test_interface.py
+++ search_service/tests/test_interface.py
@@ -6,21 +6,21 @@ import requests
 class TestInterface(unittest.TestCase):
     def test_flask_send_response(self):
         # notice that flask must run for a passing test!
-        response = requests.get('http://0.0.0.0:5000/get_suggestions?id=1')
+        response = requests.get('http://0.0.0.0:5000/suggestions?id=1')
         self.assertEqual(response.status_code, 200)
 
     def test_flask_send_json1(self):
-        response = requests.get('http://0.0.0.0:5000/get_suggestions?id=1')
+        response = requests.get('http://0.0.0.0:5000/suggestions?id=1')
         self.assertIsNotNone(response.json()[0])
 
     def test_flask_send_json2(self):
-        response = requests.get('http://0.0.0.0:5000/get_suggestions?id=1&search=hi')
+        response = requests.get('http://0.0.0.0:5000/suggestions?id=1&search=hi')
         self.assertIsNotNone(response.json()[0])
 
     def test_flask_send_json3(self):
-        response = requests.get('http://0.0.0.0:5000/get_suggestions?id=2&search=hi&start=false')
+        response = requests.get('http://0.0.0.0:5000/suggestions?id=2&search=hi&start=false')
         self.assertIsNotNone(response.json()[0])
 
     def test_flask_send_json4(self):
-        response = requests.get('http://0.0.0.0:5000/get_suggestions?search=&start=true')
+        response = requests.get('http://0.0.0.0:5000/suggestions?search=&start=true')
         self.assertIsNotNone(response.json()[0])
