310e226887c24bc83d406d3ba1b017dce6e0c071
feger <marc.feger@hhu.de>
2018-02-28 12:54:01 +0100

    Add route to init the elastic's database and start insertion listener by injection

--- search_service/interface.py
+++ search_service/interface.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, jsonify
+
+from search_service.create_database import seed_database
+from search_service.elastic.listener import start_listening
 from search_service.elastic.search import create_connection, get_suggestions, get_edits, get_duplicates_or_reasons, \
     get_all_statements_with_value
 
@@ -19,7 +21,7 @@ def suggest():
     es = create_connection()
     res = get_suggestions(es, uid, search, start)
 
-    return json.dumps(res)
+    return jsonify(result=res)
 
 
 @app.route('/edits')
@@ -30,7 +32,7 @@ def edits():
     es = create_connection()
     res = get_edits(es, uid, statement_uid, search)
 
-    return json.dumps(res)
+    return jsonify(result=res)
 
 
 @app.route('/duplicates_reasons')
@@ -41,7 +43,7 @@ def duplicates_reasons():
     es = create_connection()
     res = get_duplicates_or_reasons(es, search, uid, statement_uid)
 
-    return json.dumps(res)
+    return jsonify(result=res)
 
 
 @app.route('/statements')
@@ -51,7 +53,22 @@ def statements_with_value():
     es = create_connection()
     res = get_all_statements_with_value(es, search, uid)
 
-    return json.dumps(res)
+    return jsonify(result=res)
+
+
+@app.route('/init', methods=['POST'])
+def init():
+    app.logger.info("foo")
+    results = request.get_json(force=True)
+
+    protocol = results["DBAS_PROTOCOL"]
+    host = results["DBAS_HOST"]
+    port = results["DBAS_PORT"]
+
+    seed_database(protocol, host, port)
+    start_listening()
+
+    return jsonify(result=results)
 
 
 if __name__ == '__main__':
