0b04d51b28b3bebbe4bf08b82a823fa235d363e5
feger <marc.feger@hhu.de>
2018-02-28 10:25:40 +0100

    Reduce docker_init to a minimum; only start Flask and Elastic-Search

--- docker_init.sh
+++ docker_init.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 
-echo ":: Start docker_init"
-
-function setup_data {
-    echo ":: Setup data and interface"
-    python3.6 /code/search_service/create_database.py
-    python3.6 /code/search_service/interface.py
-}
-
-function wait_for_elastic {
-    while ! echo exit | nc 0.0.0.0 9200 > /dev/null 2>&1; do
-        echo ":: Waiting for elasticsearch on 0.0.0.0:9200"
-        sleep 1;
-    done
-    echo ":: Elastic launched on 0.0.0.0:9200"
-
-    setup_data &
-}
-
-function wait_for_dbas {
-    while ! echo exit | nc $DBAS_HOST $DBAS_PORT > /dev/null 2>&1; do
-        echo ":: Waiting for D-BAS on ${DBAS_HOST}:${DBAS_PORT}"
-        sleep 1;
-    done
-    echo ":: D-BAS launched on ${DBAS_HOST}:${DBAS_PORT}"
-}
-
-wait_for_dbas
-
-wait_for_elastic & disown
-
-python3.6 /code/search_service/elastic/listener.py & disown
+# Start Flask Server
+python3.6 /code/search_service/interface.py &
 
 echo ":: Start elasticsearch"
-
+# Start Elastic-Search
 bash /usr/local/bin/docker-entrypoint.sh
