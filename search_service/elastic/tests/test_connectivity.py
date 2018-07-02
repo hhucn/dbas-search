7a911aad5d648c1b7ebe2f507495680d2f4a0564
feger <marc.feger@uni-duesseldorf.de>
2018-07-02 13:23:51 +0200

    Test search functions

---
+++ search_service/elastic/tests/test_connectivity.py
@@ -0,0 +1,17 @@
+import unittest
+
+from search_service.elastic.search import create_connection
+
+
+class TestConnection(unittest.TestCase):
+
+    def setUp(self):
+        self.client = create_connection()
+
+    def test_connection_is_alive(self):
+        """
+        Test if the Elasticsearch is available.
+
+        :return:
+        """
+        self.assertTrue(self.client.ping())
