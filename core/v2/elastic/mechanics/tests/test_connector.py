5bb987aeafab0723ec79ceceb25d5b598b8ae44c
feger <marc.feger@uni-duesseldorf.de>
2018-07-17 13:35:45 +0200

    Fix failing test by adding random index name

--- core/v2/elastic/mechanics/tests/test_connector.py
+++ core/v2/elastic/mechanics/tests/test_connector.py
@@ -1,12 +1,19 @@
+import random
+import string
 import unittest
 
 from core import INDEX_NAME_2
 from core.v2.elastic.mechanics.es_connector import ESConnector
 
 
+def id_generator(size):
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(size))
+
+
 class TestConnector(unittest.TestCase):
     def setUp(self):
-        self.conn = ESConnector(index="banana")
+        self.index_name = id_generator(100)
+        self.conn = ESConnector(index=self.index_name)
 
     def test_default_index_name(self):
         self.assertEqual(ESConnector().index_name, INDEX_NAME_2)
@@ -15,9 +22,11 @@ class TestConnector(unittest.TestCase):
         self.assertEqual(ESConnector(index="foo").index_name, "foo")
 
     def test_create_index(self):
+        self.assertEqual(self.conn.index_name, self.index_name)
         self.conn.create_index()
         self.assertTrue(self.conn.index_exists())
 
     def test_delete_index(self):
+        self.assertEqual(self.conn.index_name, self.index_name)
         self.conn.delete_index()
         self.assertFalse(self.conn.index_exists())
