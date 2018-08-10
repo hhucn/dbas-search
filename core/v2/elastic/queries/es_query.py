8419e16db25bbd96975683d3200d4868e32fb2b0
feger <marc.feger@uni-duesseldorf.de>
2018-08-10 15:08:32 +0200

    Add simple queries to generate semantic query

--- core/v2/elastic/queries/es_query.py
+++ core/v2/elastic/queries/es_query.py
@@ -19,8 +19,8 @@ class ESQuery:
                     "query": "*" + self.text + "*" if self.text else self.text,
                     "fuzziness": self.fuzziness,
                     "analyzer": "synonyms_german",
-                    "analyze_wildcard": True,
-                    "auto_generate_synonyms_phrase_query": False
+                    "analyze_wildcard": False,
+                    "auto_generate_synonyms_phrase_query": True
                 }
             }
         }
@@ -38,13 +38,13 @@ class ESQuery:
                     "query": "*" + self.text + "*" if self.text else self.text,
                     "fuzziness": self.fuzziness,
                     "analyzer": "synonyms_english",
-                    "analyze_wildcard": True,
-                    "auto_generate_synonyms_phrase_query": False
+                    "analyze_wildcard": False,
+                    "auto_generate_synonyms_phrase_query": True
                 }
             }
         }
 
-    def german_infix_keyword_search(self, ):
+    def german_infix_keyword_search(self):
         """
         This query does a infix search with german synonyms.
 
@@ -118,6 +118,59 @@ class ESQuery:
             }
         }
 
+    def simple_match_phrase(self, analyzer: str = "synonyms_german"):
+        """
+        This is a simple query for a match_phrase with a specific analyzer.
+
+        :param analyzer: The analyzer to be used while searching
+        :return:
+        """
+        return {
+            "query": {
+                "match_phrase": {
+                    "text": {
+                        "query": self.text,
+                        "analyzer": analyzer
+                    }
+                }
+            }
+        }
+
+    def simple_query_string(self, analyzer: str = "synonyms_german"):
+        """
+        This is a simple query for a query_string with wildcards, and a specific analyzer.
+
+        :param analyzer: The analyzer to be used while searching
+        :return:
+        """
+        return {
+            "query": {
+                "query_string": {
+                    "analyzer": analyzer,
+                    "query": "*" + self.text + "*",
+                    "fields": [self.field],
+                }
+            }
+        }
+
+    def simple_fuzzy_search(self):
+        """
+        This is a simple query for fuzzy search.
+
+        :return:
+        """
+        return {
+            "query": {
+                "match": {
+                    "text": {
+                        "query": self.text,
+                        "fuzziness": "AUTO",
+                        "prefix_length": 1
+                    }
+                }
+            }
+        }
+
     def semantic_search_query(self):
         """
         This query combines all queries necessary for a semantic search.
@@ -128,12 +181,11 @@ class ESQuery:
             "query": {
                 "bool": {
                     "should": [
-                        self.german_synonym_search().get("query"),
-                        self.english_synonym_search().get("query"),
-                        self.german_infix_keyword_search().get("query"),
-                        self.english_infix_keyword_search().get("query"),
-                        self.german_infix_search().get("query"),
-                        self.english_infix_search().get("query")
+                        self.simple_match_phrase(analyzer="synonyms_english").get("query"),
+                        self.simple_match_phrase(analyzer="synonyms_german").get("query"),
+                        self.simple_query_string(analyzer="synonyms_english").get("query"),
+                        self.simple_query_string(analyzer="synonyms_german").get("query"),
+                        self.simple_fuzzy_search().get("query")
                     ]
                 }
             }
